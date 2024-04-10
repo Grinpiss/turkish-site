b1d99ae21803f2f27e97868ef1b3273b9a22367f
Gringo <vetal6-13-23@mail.ru>
2024-04-10 23:36:44 +0300

    add javascript

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
+document.addEventListener("DOMContentLoaded", function (event) { 
   //do work
-  const modal = document.querySelector ('.modal');
-  const modalBtn = document.querySelectorAll  ('[data-toggle=modal]')
+  const modal = document.querySelector('.modal');
+  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+  const closeBtn = document.querySelector('.modal__close');
+  const switchmodal = () => {
+  modal.classList.toggle('modal--visible');
+  }
 
   modalBtn.forEach09(element => {
-    element.addEventListener('click', () => {
-    modal.classList.toggle ('modal--visible')
-    });
-});
+    element.addEventListener('click', switchmodal); 
+   });
+    
+    closeBtn.addEventListener('click', switchmodal); 
+
 });
