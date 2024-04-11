7a60b60e81f658380de394735c32b340e41e2340
Gringo <vetal6-13-23@mail.ru>
2024-04-11 18:01:37 +0300

    fixed btn and add jqery and add function

--- js/main.js
+++ js/main.js
@@ -1,15 +1,24 @@
-
-
 $(document).ready(function () {
   var modal = $('.modal'),
       modalBtn = $('[data-toggle=modal]'),
       closelBtn = $('.modal__close');
-  modalBt.on('click', function () {
+  modalBtn.on('click', function () {
     modal.toggleClass('modal--visible');
   });
   closelBtn.on('click', function () {
     modal.toggleClass('modal--visible');
   }); 
-    
-}); 
+
+  var mySwiper = new Swiper ('.swiper-container', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+});
 
