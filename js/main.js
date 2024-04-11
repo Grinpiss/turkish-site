39f824c743baa1a75447bf9d360771dd3582ecab
Gringo <vetal6-13-23@mail.ru>
2024-04-11 13:54:42 +0300

    add jqery

--- js/main.js
+++ js/main.js
@@ -1,16 +1,14 @@
-document.addEventListener("DOMContentLoaded", function (event) { 
-  //do work
-  const modal = document.querySelector('.modal');
-  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-  const closeBtn = document.querySelector('.modal__close');
-  const switchmodal = () => {
-  modal.classList.toggle('modal--visible');
-  }
 
-  modalBtn.forEach09(element => {
-    element.addEventListener('click', switchmodal); 
-   });
-    
-    closeBtn.addEventListener('click', switchmodal); 
 
+$(document).ready(function () {
+  var modal = $('.modal'),
+      modalBtn = $('[data-toggle=modal]'),
+      closelBtn = $('.modal__close');
+  modalBt.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
+  closelBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
 });
+
